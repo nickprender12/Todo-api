2cd9261bbf389d94cd48196da0e0e68ac32db1f7
Nicholas Prendergast <nickprender@gmail.com>
2021-03-12 13:04:25 -0800

    Update SECRET and scripts

--- utils/config.js
+++ utils/config.js
@@ -1,21 +1,20 @@
 require('dotenv').config();
 
 const { PORT } = process.env || 5000;
-// let { MONGODB_URI } = process.env;
-const { MONGODB_URI } = process.env;
-
+let { MONGODB_URI } = process.env;
 const { SECRET } = process.env;
-// if (process.env.NODE_ENV === 'test') {
-//   MONGODB_URI = process.env.TEST_URI;
-// }
 
-// if (process.env.NODE_ENV === 'production') {
-//   MONGODB_URI = process.env.ATLAS_URI;
-// }
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_URI;
+}
 
-// if (process.env.NODE_ENV === 'development') {
-//   MONGODB_URI = process.env.LOCAL_URI;
-// }
+if (process.env.NODE_ENV === 'production') {
+  MONGODB_URI = process.env.MONGODB_URI;
+}
+
+if (process.env.NODE_ENV === 'development') {
+  MONGODB_URI = process.env.LOCAL_URI;
+}
 
 module.exports = {
   MONGODB_URI,
